7c5ee456477c64421251f47093fa952a5caeeafc
Quoc Bao <Baohq@aiaracorp.com>
2023-10-27 14:45:23 +0700

    p;7;

---
+++ Assets/03_Scripts/General Manager/StudioTipsSC.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudioTipsSC
+{
+    private string tips01 = "Tip 1";
+    private string tips02 = "Tip 2";
+    private string tips03 = "Tip 3";
+    private string tips04 = "Tip 4";
+    private string tips05 = "Tip 5";
+    private string tips06 = "Tip 6";
+    private string tips07 = "Tip 7";
+    private string tips08 = "Tip 8";
+    private string tips09 = "Tip 9";
+    private string tips10 = "Tip 10";
+    private string tips11 = "Tip 11";
+    private string tips12 = "Tip 12";
+    private string tips13 = "Tip 13";
+    private string tips14 = "Tip 14";
+    private string tips15 = "Tip 15";
+    public void TipsToShow(int tipsOder)
+    {   
+    }
+}
